bf254b8491858b309e8223d997f1f72731276e43
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2024-03-05 22:56:52 +0100

    modified Cell.h, remeber to fix todos

--- src/Cell.h
+++ src/Cell.h
@@ -3,31 +3,37 @@
 
 #include <list>
 #include <memory>
+#include <QtWidgets/QTableWidgetItem>
 
 #include "Subject.h"
 // #include "Formula.h"
 
-class Cell: public Subject, public Observer{ // every cell is both a Subject and an Observer
+class Cell : public Subject, public Observer, public QTableWidgetItem {
+    // every cell is both a Subject and an Observer
 public:
 
-
+    // TODO: find how the constructor must be
     ~Cell() override = default;
 
-    void setValue(float v);
-    double getValue() const;
-    // from subject
-    void notify() override ; // virtual is redundant
-    void subscribe(Observer* o)override ;
-    void unsubscribe(Observer* o)override ;
+    void setValue(double v);
+    const double getValue() const;
 
+    void setCustomValue(bool cv);
+    const bool checkCustomValue() const;
+
+    // from subject and observer
+
+    void notify() override; // virtual is redundant
+    void update() override;
+    void subscribe(Observer *o) override;
+    void unsubscribe(Observer *o) override;
 
 private:
+    // TODO find QT variables
     double value;
-    bool hasValue;
-    std::list<std::shared_ptr<Cell>> cellSelection; // list of shared pointers -> I want to be able to do a cell selection
-    std::list<Observer*> observers;
-
-
+    bool customValue; // variable to check if the cell has been modified since its creation
+    std::list<std::shared_ptr<Cell> > cellSelection; // list of shared pointers to involved cells-> I want to be able to do a cell selection
+    std::list<Observer *> observers;
 };
 
 #endif //QT_SPREADSHEET_CELL_H
