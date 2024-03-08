a678a151b7b870f37e021ef3cf25be0f783e21e9
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2024-03-08 18:50:03 +0100

    started defining spreasheet methods

--- src/Spreadsheet.h
+++ src/Spreadsheet.h
@@ -2,9 +2,19 @@
 #define SPREADSHEET_H
 
 #include <QtWidgets/QTableWidget>
+#include "Cell.h"
 
 class Spreadsheet: public QTableWidget{
+public:
+    explicit Spreadsheet(QWidget* parent = nullptr);
+    Spreadsheet(int rows, int cols, QWidget* parent = nullptr);
+    void setColumnTitle(int colIndex, const QString& title);
 
+    const int getRows() const;
+    const int getColumns() const;
+
+
+private:
 };
 
 
