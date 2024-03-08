590b780ae992ef5c60ebe2f851b4675c75644934
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2024-03-08 18:50:59 +0100

    started defining spreadsheet methods

--- src/Spreadsheet.cpp
+++ src/Spreadsheet.cpp
@@ -1,1 +1,26 @@
 #include "Spreadsheet.h"
+
+Spreadsheet::Spreadsheet(QWidget *parent): QTableWidget(parent) {}
+
+Spreadsheet::Spreadsheet(int rows, int cols, QWidget *parent): QTableWidget(parent) {
+    setRowCount(rows);
+    setColumnCount(cols);
+    for (int i = 0; i<rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            Cell *cell = new Cell;
+            this->setItem(i, j, cell);
+        }
+    }
+}
+
+void Spreadsheet::setColumnTitle(int colIndex, const QString &title) {
+    setHorizontalHeaderItem(colIndex, new QTableWidgetItem(title, QTableWidgetItem::Type));
+}
+
+const int Spreadsheet::getRows() const {
+    return rowCount();
+}
+
+const int Spreadsheet::getColumns() const {
+    return columnCount();
+}
